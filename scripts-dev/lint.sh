008ac9b7a6bd668134dd10ed386243840b154896
Andrew Morgan <andrew@amorgan.xyz>
2021-01-10 21:55:40 -0500

    Ensure we run linting on test files

--- scripts-dev/lint.sh
+++ scripts-dev/lint.sh
@@ -11,7 +11,7 @@ if [ $# -ge 1 ]
 then
     files=$*
   else
-    files="my_project_name my-project-name"
+    files="my_project_name my-project-name tests"
 fi
 
 echo "Linting these locations: $files"
